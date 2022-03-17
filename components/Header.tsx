08c4b4dc51ac886fb99beedf2bc81cba14429d7b
accel33 <accelwtf@gmail.com>
2022-03-16 21:55:32 -0500

    avance

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,14 +3,19 @@ import Link from 'next/link'
 function Header() {
    return (
       <header>
-         <div className="">
+         <div className="icono-y-navbar flex items-center space-x-5">
             <Link href="/">
                <img
                   src="https://links.papareact.com/yvf"
                   alt="medium"
-                  className=""
+                  className="w-44 object-contain cursor-pointer"
                />
             </Link>
+            <div className='hidden md:inline-flex items-center space-x-5'>
+               <h3>About</h3>
+               <h3>Contact</h3>
+               <h3 className='bg-green-600 px-4 py-1 rounded-full'>Follow</h3>
+            </div>
          </div>
          <div className=""></div>
       </header>
